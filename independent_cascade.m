2adbd18d0977d193fb8adad60a20f43a55066c92
neophyte226 <sdasari@ncsu.edu>
2017-12-04 16:25:00 -0500

    Fixed a Bug and improved efficiency of the algorithm

--- independent_cascade.m
+++ independent_cascade.m
@@ -1,4 +1,4 @@
-function [active_nodes] = independent_cascade(G,active_rounds,initial_nodes,activation_probability)
+function [active_nodes,G_duplicate] = independent_cascade(G,active_rounds,initial_nodes,activation_probability)
 %G-Graph Data Structure
 %active rounds-number of active rounds to run the diffusion process for
 %initial_nodes-seeds for activation
@@ -7,25 +7,37 @@ function [active_nodes] = independent_cascade(G,active_rounds,initial_nodes,acti
 active_nodes=initial_nodes;
 G_duplicate=G;
 prev_length=0;
-while((length(active__nodes)~=prev_length) || length(active_nodes)==size(G.Nodes,1) )
-    active_nodes_append=[];
-    prev_length=length(active_nodes);
-    for individual = 1:length(active_nodes)
+edges=0;
+% while((length(active_nodes)~=prev_length) || length(active_nodes)==size(G.Nodes,1) )
+for i=1:active_rounds
+    if i==1
+        active_nodes_last_round=active_nodes;
+    else
+        active_nodes_last_round=active_nodes_current_round;
+    end
+    active_nodes_current_round=[];
+    prev_length=length(active_nodes)
+    for individual = 1:length(active_nodes_last_round)
     
-        potential_infected=neighbors(G_duplicate,active_nodes(individual));
-        Acommon = intersect(potential_infected,active_nodes);
+        potential_infected=neighbors(G_duplicate,active_nodes_last_round(individual));
+        Acommon = intersect(potential_infected,active_nodes_last_round);
            potential_infected = setxor(potential_infected,Acommon);
         for victim = 1:length(potential_infected)
             temp=rand;
             if(activation_probability>temp)
-                active_nodes_append=[active_nodes_append;potential_infected(victim)];
+                active_nodes_current_round=[active_nodes_current_round;potential_infected(victim)];
+                edges=edges+1;
+                 G_duplicate=G_duplicate.rmedge(active_nodes_last_round(individual),potential_infected(victim));
+
             else
-                G_duplicate=G.rmedge(active_nodes(individual),potential_infected(victim));
+                edges=edges+1;
+                G_duplicate=G_duplicate.rmedge(active_nodes_last_round(individual),potential_infected(victim));
             end
             
             
              
         end
     end
-    active_nodes=[active_nodes;active_nodes_append];
+    active_nodes=[active_nodes;active_nodes_current_round];
+    length(active_nodes)
 end
