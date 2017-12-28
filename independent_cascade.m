7f316ccc15ea9e40e2d3edf02a397e879b8ff46e
bhargavsaidasari <sdasari@ncsu.edu>
2017-12-27 21:49:52 -0500

    Fixed Bugs

--- independent_cascade.m
+++ independent_cascade.m
@@ -1,4 +1,4 @@
-function [active_nodes,G_duplicate] = independent_cascade(G,active_rounds,initial_nodes,activation_probability)
+function [total_infected] = independent_cascade(G,active_rounds,initial_nodes,activation_probability)
 %G-Graph Data Structure
 %active rounds-number of active rounds to run the diffusion process for
 %initial_nodes-seeds for activation
@@ -8,9 +8,10 @@ active_nodes=initial_nodes;
 G_duplicate=G;
 prev_length=0;
 edges=0;
-% while((length(active_nodes)~=prev_length) || length(active_nodes)==size(G.Nodes,1) )
-for i=1:active_rounds
-    if i==1
+round=1 ;
+ while((length(active_nodes)~=prev_length) && length(active_nodes)~=size(G.Nodes,1) )
+% for i=1:active_rounds
+    if round==1
         active_nodes_last_round=active_nodes;
     else
         active_nodes_last_round=active_nodes_current_round;
@@ -20,7 +21,7 @@ for i=1:active_rounds
     for individual = 1:length(active_nodes_last_round)
     
         potential_infected=neighbors(G_duplicate,active_nodes_last_round(individual));
-        Acommon = intersect(potential_infected,active_nodes_last_round);
+        Acommon = intersect(potential_infected,active_nodes);
            potential_infected = setxor(potential_infected,Acommon);
         for victim = 1:length(potential_infected)
             temp=rand;
@@ -38,6 +39,8 @@ for i=1:active_rounds
              
         end
     end
-    active_nodes=[active_nodes;active_nodes_current_round];
-    length(active_nodes)
-end
+    active_nodes=[active_nodes;unique(active_nodes_current_round)];
+    length(active_nodes);
+    round=round+1;
+ end
+total_infected=size(active_nodes,1);
